cb12935066daa8d878f05643d9206d924f2e833f
Bilun Sun <bilunsun@gmail.com>
2022-11-04 12:02:39 -0400

    Seems to work. Need to test.

--- src/mcts.rs
+++ src/mcts.rs
@@ -1,21 +1,10 @@
-use rand::Rng;
-use std::rc::Rc;
 use std::time::Instant;
 
 use crate::board::{Action, Board, Outcome, Player};
 use crate::utils::get_random_action;
 
-type Reward = f32;
-
-// pub fn outcome_to_reward(outcome: Outcome, turn: Player) -> Reward {
-//     match outcome {
-//         Outcome::Winner(winner) => match winner {
-//             turn => 1.0,
-//             _ => -1.0,
-//         },
-//         Outcome::Draw => 0.0,
-//     }
-// }
+const SQRT_TWO: f32 = 1.41421356237;
+// const SQRT_TWO: f32 = 0.1;
 
 pub fn rollout(board: &mut Board) -> Outcome {
     while !board.is_game_over() {
@@ -58,14 +47,37 @@ impl Node {
         }
     }
 
+    pub fn uct(&self, parent_visit_count: usize) -> f32 {
+        if self.visit_count == 0 {
+            return f32::INFINITY;
+        }
+        let exploitation_term = self.value / (self.visit_count as f32);
+        let exploration_term =
+            SQRT_TWO * f32::sqrt(f32::ln(parent_visit_count as f32) / (self.visit_count as f32));
+        exploitation_term + exploration_term
+    }
+
+    pub fn update(&mut self, outcome: Outcome) {
+        match outcome {
+            Outcome::Winner(winner) => {
+                if winner != self.turn {
+                    self.value += 1.0
+                }
+            }
+            _ => (),
+        };
+
+        self.visit_count += 1;
+    }
+
     pub fn get_best_child(&mut self) -> Option<&mut Node> {
         let mut best_score: f32 = f32::NEG_INFINITY;
         let mut best_child: Option<&mut Node> = None;
 
         for child in &mut self.children {
-            let score = 0.0;
-            if score > best_score {
-                best_score = score;
+            let child_score = child.uct(self.visit_count);
+            if child_score > best_score {
+                best_score = child_score;
                 best_child = Some(child);
             }
         }
@@ -73,10 +85,10 @@ impl Node {
         best_child
     }
 
-    pub fn expand(&mut self, board: &Board) {
+    pub fn expand(&mut self, board: &Board) -> Option<&mut Node> {
         if board.is_game_over() {
             self.state = State::Terminal;
-            return;
+            return None;
         }
 
         // Create a child node for all untried moves
@@ -89,45 +101,41 @@ impl Node {
         // // Randomly select a child node
         // let random_index = rand::thread_rng().gen_range(0..self.children.len());
         // Some(&mut self.children[random_index])
+
+        Some(&mut self.children[0])
     }
 
     pub fn iteration(&mut self, board: &mut Board) {
-        // let mut parents: Vec<Rc<&mut Node>> = Vec::new();
         let mut parents_pointers: Vec<*mut Node> = Vec::new();
 
         // Selection
         let mut node = self;
         while !node.children.is_empty() {
-            // parents.push(Rc::clone(&Rc::new(node)));
             parents_pointers.push(node);
             node = node.get_best_child().unwrap();
+            board.make_action(node.action.unwrap()).ok();
         }
 
-        // Expansion
-        node.expand(board);
+        // if node.action.unwrap() == 18 {
+        //     assert!(board.is_game_over());
+        // }
+        // if node.action.is_none() {
+        //     dbg!(&node);
+        // }
 
-        // Rollout
-        let outcome = rollout(board);
-        let reward = 1.0;
-        // let reward = match outcome {
-        //     Outcome::Winner(winner) => {
-        //         if winner == self.turn {
-        //             1
-        //         } else {
-        //             -1
-        //         }
-        //     }
-        //     Outcome::Draw => 0,
-        // };
+        // Expansion
+        let outcome = match node.expand(board) {
+            Some(_) => rollout(board), // Rollout
+            None => board
+                .outcome
+                .expect("Terminal state should have an outcome."),
+        };
 
         // Backpropagate
-        node.visit_count += 1;
-        node.value += reward;
+        node.update(outcome);
         for parent_pointer in parents_pointers.iter().rev() {
-            let mut parent = unsafe { parent_pointer.as_mut().unwrap() };
-            parent.visit_count += 1;
-            parent.value += reward;
-            // dbg!(&parent.visit_count);
+            let parent = unsafe { parent_pointer.as_mut().unwrap() };
+            parent.update(outcome);
         }
     }
 }
@@ -144,21 +152,33 @@ impl MCTS {
         Self { root, board }
     }
 
-    pub fn get_best_action(&mut self, n_iterations: usize) {
-        for i in 0..n_iterations {
+    pub fn get_best_action(&mut self, n_iterations: usize) -> Action {
+        for _ in 0..n_iterations {
             let mut board = self.board.clone();
             self.root.iteration(&mut board);
-            // self.root.iteration(&mut self.board);
         }
-        // for child in &self.root.children {
-        //     dbg!(&child);
+
+        // // For debugging only
+        // for child in &mut self.root.children {
+        //     println!(
+        //         "{} -> {}",
+        //         child.action.unwrap(),
+        //         child.uct(self.root.visit_count)
+        //     );
         // }
+
+        // dbg!(&self.root);
+
+        let best_child = self
+            .root
+            .get_best_child()
+            .expect("Root node should have children.");
+        best_child.action.expect("Child should have action")
     }
 }
 
 pub fn test_mcts() {
-    let mut board = Board::new(10, 5);
-    println!("{board}");
+    let mut board = Board::new(15, 5);
 
     for move_string in vec!["B2", "A2", "C3", "A1", "A3", "B3"].iter() {
         let action = board
@@ -168,18 +188,20 @@ pub fn test_mcts() {
     }
     println!("{board}");
 
-    let mut mcts = MCTS::new(&board);
-    let now = Instant::now();
-    let best_action = mcts.get_best_action(1_000);
-
-    // let n_iterations = 1_000;
     // let mut mcts = MCTS::new(&board);
-    // let now = Instant::now();
     // let best_action = mcts.get_best_action(1_000);
 
-    // let elapsed_s = now.elapsed().as_secs_f32();
-    // println!(
-    //     "Iterations per second: {}",
-    //     (n_iterations as f32 / elapsed_s) as usize
-    // );
+    // board.make_action(best_action).ok();
+    // println!("{board}");
+
+    let n_iterations = 50_000;
+    let mut mcts = MCTS::new(&board);
+    let now = Instant::now();
+    mcts.get_best_action(n_iterations);
+
+    let elapsed_s = now.elapsed().as_secs_f32();
+    println!(
+        "Iterations per second: {}",
+        (n_iterations as f32 / elapsed_s) as usize
+    );
 }
